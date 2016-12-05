fe1832e75434c6c7f566a2fdf22660145d9aa130
marko <marko.nou@gmail.com>
2016-12-05 20:22:53 +0200

    Remove assert-related code

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define __ASSERT_USE_STDERR
 #include <assert.h>
 #include <avr/io.h>
 #include <util/delay.h>
@@ -19,29 +18,12 @@ void main (void)
             GIT_DESCR, __DATE__, __TIME__);
     fprintf(stderr, "avr-libc version: %s\n", __AVR_LIBC_VERSION_STRING__);
     /* End UART3 init and info print */
-    /* Test assert - REMOVE IN FUTURE LABS */
-    char *array;
-    uint32_t i = 1;
-    extern int __heap_start, *__brkval;
-    int v;
-    array = malloc( i * sizeof(char));
-    assert(array);
-    /* End test assert */
+   
 
     while (1) {
         /* Set pin 3 high to turn LED on */
         PORTA |= _BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
-        /* Test assert - REMOVE IN FUTURE LABS */
-        /*
-         * Increase memory allocated for array by 100 chars
-         * until we have eaten it all and print space between Stack and Heap.
-         * Thats how assert works in run-time.
-         */
-        array = realloc( array, (i++ * 100) * sizeof(char));
-        fprintf(stderr, "%d ",
-                (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval));
-        assert(array);
         /* End test assert */
         /* Set pin 3 low to turn LED off */
         PORTA &= ~_BV(PORTA3);
