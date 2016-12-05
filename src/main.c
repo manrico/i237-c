00b210cf3f2f713220ece145671247e827c8e36a
marko <marko.nou@gmail.com>
2016-12-06 01:45:18 +0200

    Add LCD functionality

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "uart.h"
 #include "hmi_msg.h"
 #include "print_helper.h"
+#include "../lib/hd44780_111/hd44780.h"
 
 #define BLINK_DELAY_MS 100
 
@@ -17,11 +18,13 @@ void main (void)
     stderr = &uart3_out;
     uart0_init();
     stdout = stdin = &uart0_io;
+    lcd_init();
 
     fprintf_P(stderr, PSTR(MSG_PROGRAM_VERSION), PSTR(GIT_DESCR), PSTR(__DATE__), PSTR(__TIME__));
     fprintf_P(stderr, PSTR(MSG_LIBC_VERSION), PSTR(__AVR_LIBC_VERSION_STRING__));
     fprintf_P(stdout, PSTR(MSG_STUD_NAME "\n"));
     print_ascii_tbl(stdout);
+    lcd_puts_P(PSTR(MSG_STUD_NAME));
 
     unsigned char table[128];
     for (unsigned char a = 0; a < sizeof(table); a++) {
@@ -34,16 +37,20 @@ void main (void)
         PORTA |= _BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
 
+        lcd_goto(0x40);
         char input;
         fprintf_P(stdout, PSTR("\n" MSG_ENTER_MONTH));
         fscanf(stdin, "%c", &input);
-        for (int i = 0 ; i < 6 ; i++){
+        for (int i = 0 ; i < 6 ; i++) { //sizeof months does not work?
             if (!strncmp_P(&input,  (PGM_P) pgm_read_word(&months[i]), 1)) {
                 fputc('\n', stdout);
                 fprintf_P(stdout, (PGM_P) pgm_read_word(&months[i]));
+                lcd_puts_P((PGM_P)pgm_read_word(&months[i]));
+                lcd_putc(' ');
             }
         }
-        
+        lcd_puts_P(PSTR("                ")); // could not figure out the size, so putting 16 empty spaces here :(
+
         /*cycle end led*/
         PORTA &= ~_BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
