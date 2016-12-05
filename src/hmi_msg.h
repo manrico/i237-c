95adf8b51369c39795f8cb659e46f8990bfad4cf
marko <marko.nou@gmail.com>
2016-12-05 23:41:07 +0200

    Move messages to header file, ask month name

--- src/hmi_msg.h
+++ src/hmi_msg.h
@@ -1,2 +1,5 @@
-#define PROGRAM_VERSION "Version: %s built on: %s %s\n"
-#define LIBC_VERSION "avr-libc version: %s\n"
+#define MSG_PROGRAM_VERSION "Version: %s built on: %s %s\n"
+#define MSG_LIBC_VERSION "avr-libc version: %s\n"
+#define MSG_STUD_NAME "Marko Nõu"
+#define MSG_ENTER_MONTH "Enter first letter of month name >"
+const char *months[6] = {"January", "February", "March", "April", "May", "June"};
--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <string.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include "uart.h"
@@ -13,27 +13,41 @@ void main (void)
 {
     /* Set pin 3 of PORTA for output */
     DDRA |= _BV(DDA3);
-    /* Init error console as stderr in UART3 and print user code info */
+    /* Init error console as stderr in UART3 and print version and lib info */
     uart3_init();
     stderr = &uart3_out;
-    fprintf(stderr, PROGRAM_VERSION,
-            GIT_DESCR, __DATE__, __TIME__);
-    fprintf(stderr, LIBC_VERSION, __AVR_LIBC_VERSION_STRING__);
+    fprintf(stderr, MSG_PROGRAM_VERSION, GIT_DESCR, __DATE__, __TIME__);
+    fprintf(stderr, MSG_LIBC_VERSION, __AVR_LIBC_VERSION_STRING__);
     /* End UART3 init and info print */
 
-    print_ascii_tbl(stderr);
+    /* Init UART0 and print out student's name */
+    uart0_init();
+    stdout = stdin = &uart0_io;
+    fprintf(stdout, MSG_STUD_NAME "\n");
+    /* END UART0 init*/
+
+    print_ascii_tbl(stdout);
 
     unsigned char table[128];
     for (unsigned char a = 0; a < sizeof(table); a++) {
         table[a] = a;
     }
-    print_for_human(stderr, table, sizeof(table));
+    print_for_human(stdout, table, sizeof(table));
 
     while (1) {
         /* Set pin 3 high to turn LED on */
         PORTA |= _BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
-        /* End test assert */
+
+        char input;
+        fprintf(stdout, "\n" MSG_ENTER_MONTH);
+        fscanf(stdin, "%c", &input);
+        for (int i = 0 ; i < 6 ; i++){
+            if (!strncmp(&input, months[i],1)) {
+                fprintf(stdout, "\n%s", months[i]);
+            }
+        }
+
         /* Set pin 3 low to turn LED off */
         PORTA &= ~_BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
